33aa5b0c8345a5c1ce34d826c6346df722fb1203
LHennep <302468696@student.rocmondriaan.nl>
2024-06-04 15:36:19 +0200

    Restart
    in deze commit heb ik alles verwijdert en better gemakt

---
+++ main.js
@@ -0,0 +1,30 @@
+// Typing effect script
+const typingElement = document.getElementById('typing');
+const textArray = ['Leonerfy Hennep', 'a Full Stack Developer'];
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typingElement.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 200);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingElement.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 100);
+  } else {
+    textArrayIndex = (textArrayIndex + 1) % textArray.length;
+    setTimeout(type, 1000);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (textArray.length) setTimeout(type, 500);
+});
